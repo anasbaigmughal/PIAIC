9a83fe7391b6d0b85ba3f3656d0f1b8a43372713
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-05-11 11:06:56 +0500

    task2

---
+++ Assignment1_PIAIC51686/task02.js
@@ -0,0 +1,10 @@
+var firstName = "Muhammad Anas";
+var lastName = "Baig";
+var age = 22;
+var cityName = "Islamabad";
+var countryName = "Pakistan";
+
+alert("Name: " + (firstName + " " + lastName));
+alert("Age: " + age + " years old.");
+alert("City: " + cityName);
+alert("Country: " + countryName);
