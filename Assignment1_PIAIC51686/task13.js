0c46d1275310b0a48f9bc77aa1938254a6ade3e2
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-05-31 22:22:25 +0500

    task13

---
+++ Assignment1_PIAIC51686/task13.js
@@ -0,0 +1,19 @@
+var a = 10;
+document.write("Result:");
+document.write("<br />The value of a is: " + a);
+document.write("<br />-------------------------------<br />");
+
+document.write("<br />The value of ++a is: " + ++a);
+document.write("<br />Now the value of a is: " + a);
+document.write("<br /><br />");
+
+document.write("<br />The value of a++ is: " + a++);
+document.write("<br />Now the value of a is: " + a);
+document.write("<br /><br />");
+
+document.write("<br />The value of --a is: " + --a);
+document.write("<br />Now the value of a is: " + a);
+document.write("<br /><br />");
+
+document.write("<br />The value of a-- is: " + a--);
+document.write("<br />Now the value of a is: " + a);
